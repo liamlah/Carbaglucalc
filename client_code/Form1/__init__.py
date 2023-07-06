a4e3ae3017e2abe0505eac78ec50583e63aa9016
anvil@liamjones.org <anvil@liamjones.org>
2023-07-06 13:22:28 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+from form_checker import validation
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,15 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    self.validator = validation.Validator()
+    self.validator.require_text_field(self.answerbox)
+    self.validator.enable_when_valid(self.calculate)
+
+    
+  def calculate_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if int(self.ammonia_factor) < 0 or int(self.ammonia_factor) >1000:
+      alert(f'Ammonia must be a positive number less than 1000')
+    
+    pass
+
